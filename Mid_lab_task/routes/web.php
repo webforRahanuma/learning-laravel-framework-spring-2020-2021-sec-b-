a8bf3f725083b383344317ae2e06497af2873be6
webforRahanuma <rahanumatasnim98@gmail.com>
2021-03-05 10:26:59 +0600

    Login module commit

--- Mid_lab_task/routes/web.php
+++ Mid_lab_task/routes/web.php
@@ -12,5 +12,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo "welcome";
 });
+
+
+Route::get('/login', 'LoginController@index');
+Route::post('/login', 'LoginController@verify');
+Route::get('/logout', 'LogoutController@index');
+//Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware'=> 'sess'], function(){
+    Route::get('/home', ['uses'=>'HomeController@index']);
+    
+    Route::get('/home/create', 'HomeController@create')->middleware('sess')->name('home.create');
+    Route::post('/home/create', 'HomeController@store');
+    Route::get('/home/userlist', 'HomeController@userlist')->name('home.userlist');
+    Route::get('/home/edit/{id}', 'HomeController@edit')->name('home.edit');
+    Route::post('/home/edit/{id}', 'HomeController@update');
+    Route::get('/home/delete/{id}', 'HomeController@delete');
+    Route::post('/home/delete/{id}', 'HomeController@destroy');
+    Route::get('/home/details/{id}', 'HomeController@show');
+
+    Route::resource('/product', 'ProductController');
+    //Route::get('/product/abc/{id}', 'ProductController@xyz');
+});
+
+
